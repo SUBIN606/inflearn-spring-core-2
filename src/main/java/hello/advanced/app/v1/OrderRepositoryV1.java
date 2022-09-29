81971946d0420255654124ea5fa558311f1b740f
subin606 <gaemilee606@gmail.com>
2022-09-29 16:26:22 +0900

    로그 추적기 V1 적용

---
+++ src/main/java/hello/advanced/app/v1/OrderRepositoryV1.java
@@ -0,0 +1,27 @@
+package hello.advanced.app.v1;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class OrderRepositoryV1 {
+
+    public void save(String itemId) {
+        // 저장로직
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외 발생!");
+        }
+        // 일반적으로 상품을 저장할 때 1초(1000ms)가 걸린다고 가정한다.
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/hello/advanced/app/v1/OrderServiceV1.java
@@ -0,0 +1,16 @@
+package hello.advanced.app.v1;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class OrderServiceV1 {
+
+    private final OrderRepositoryV1 orderRepository;
+
+    public void orderItem(String itemId) {
+        orderRepository.save(itemId);
+    }
+
+}
