24b2d690dfadad4dcce1a7898dc9f04d06feb425
subin606 <gaemilee606@gmail.com>
2022-09-29 16:52:43 +0900

    로그 추적기 V1 적용 누락된 부분 추가

--- src/main/java/hello/advanced/app/v1/OrderRepositoryV1.java
+++ src/main/java/hello/advanced/app/v1/OrderRepositoryV1.java
@@ -1,5 +1,7 @@
 package hello.advanced.app.v1;
 
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -7,13 +9,22 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class OrderRepositoryV1 {
 
+    private final HelloTraceV1 trace;
+
     public void save(String itemId) {
-        // 저장로직
-        if (itemId.equals("ex")) {
-            throw new IllegalStateException("예외 발생!");
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderRepository.save()");
+            // 저장로직
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생!");
+            }
+            // 일반적으로 상품을 저장할 때 1초(1000ms)가 걸린다고 가정한다.
+            sleep(1000);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
         }
-        // 일반적으로 상품을 저장할 때 1초(1000ms)가 걸린다고 가정한다.
-        sleep(1000);
     }
 
     private void sleep(int millis) {
