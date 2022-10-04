2e241aedba275d4840fd08aede6321f821888a38
subin606 <gaemilee606@gmail.com>
2022-10-04 22:33:54 +0900

    인터페이스 기반 프록시 적용

---
+++ proxy/src/main/java/hello/proxy/config/v1_proxy/InterfaceProxyConfig.java
@@ -0,0 +1,41 @@
+package hello.proxy.config.v1_proxy;
+
+import hello.proxy.app.v1.OrderControllerV1;
+import hello.proxy.app.v1.OrderControllerV1Impl;
+import hello.proxy.app.v1.OrderRepositoryV1;
+import hello.proxy.app.v1.OrderRepositoryV1Impl;
+import hello.proxy.app.v1.OrderServiceV1;
+import hello.proxy.app.v1.OrderServiceV1Impl;
+import hello.proxy.config.v1_proxy.interface_proxy.OrderControllerInterfaceProxy;
+import hello.proxy.config.v1_proxy.interface_proxy.OrderRepositoryInterfaceProxy;
+import hello.proxy.config.v1_proxy.interface_proxy.OrderServiceInterfaceProxy;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 실제 객체는 스프링 빈으로 등록하지 않고, 프록시를 대신 등록한다.
+ * 프록시는 실제 구현체를 참조하기 때문에 프록시를 통해 실제 구현체를 호출할 수 있다.
+ */
+@Configuration
+public class InterfaceProxyConfig {
+
+    @Bean
+    public OrderControllerV1 orderController(LogTrace logTrace) {
+        OrderControllerV1Impl controllerImpl = new OrderControllerV1Impl(orderService(logTrace));
+        return new OrderControllerInterfaceProxy(controllerImpl, logTrace);
+    }
+
+    @Bean
+    public OrderServiceV1 orderService(LogTrace logTrace) {
+        OrderServiceV1Impl serviceImpl = new OrderServiceV1Impl(orderRepository(logTrace));
+        return new OrderServiceInterfaceProxy(serviceImpl, logTrace);
+    }
+
+    @Bean
+    public OrderRepositoryV1 orderRepository(LogTrace logTrace) {
+        OrderRepositoryV1Impl repositoryImpl = new OrderRepositoryV1Impl();
+        return new OrderRepositoryInterfaceProxy(repositoryImpl, logTrace);
+    }
+
+}
---
+++ proxy/src/main/java/hello/proxy/config/v1_proxy/interface_proxy/OrderControllerInterfaceProxy.java
@@ -0,0 +1,35 @@
+package hello.proxy.config.v1_proxy.interface_proxy;
+
+import hello.proxy.app.v1.OrderControllerV1;
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class OrderControllerInterfaceProxy implements OrderControllerV1 {
+
+    private final OrderControllerV1 target;
+    private final LogTrace logTrace;
+
+    @Override
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin("OrderController.orderItem()");
+            // target 호출
+            String result = target.request(itemId);
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+
+    /** 로그를 출력하지 않기 때문에 단순하게 위임한 한다. */
+    @Override
+    public String noLog() {
+        return target.noLog();
+    }
+
+}
